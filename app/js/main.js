7731d439e368c4f7ec75cc77acd63ce705ed0d43
Zvirum <nastena5dm@mail.ru>
2020-06-30 19:42:25 +0300

    added header/slider

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.slider').slick({
+    // autoplay: true,
+    // autoplaySpeed: 5000,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+  }); 
+});
