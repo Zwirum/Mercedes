b8ca0f35a0564a01b6ac2b4a7a3725d2a47df41a
Zvirum <nastena5dm@mail.ru>
2020-07-01 19:35:38 +0300

    added models/service/form/footer sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,18 @@
 $(function(){
   $('.slider').slick({
-    // autoplay: true,
-    // autoplaySpeed: 5000,
+    autoplay: true,
+    autoplaySpeed: 5000,
     arrows: false,
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
   }); 
+  $('.services__tubs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.services__tubs').find('.tab-item').removeClass('active-tab').hide();
+      $('.services__tubs .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
 });
